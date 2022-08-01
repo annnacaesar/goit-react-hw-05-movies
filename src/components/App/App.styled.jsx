b879246d548e8e22ff73e7c0cf75e4fd58a4446b
annnacaesar <Anechka22334@gmail.com>
2022-08-01 15:44:57 +0300

    Create App.styled.jsx

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,8 @@
+// import styled from 'styled-components';
+
+// export const AppContainer = styled.div`
+// 	font-size: 40px;
+// 	color: #010101;
+// 	padding: 50px;
+// 	background-image: var(--background-gradient);
+// `;
